51ba172d3bc337799a43b9f0775f87bc76aa406b
BeatriceBRIAND <b.briand@mistermenuiserie.com>
2020-12-28 14:40:32 +0100

    commit amelioration exp utilisateur

--- Components/Search.js
+++ Components/Search.js
@@ -1,6 +1,6 @@
 //Components/Search.js
 import React from 'react'
-import { StyleSheet, Button, TextInput, View, FlatList } from 'react-native'
+import { StyleSheet, Button, TextInput, View, FlatList, ActivityIndicator } from 'react-native'
 //import film from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' // import { } from ... car c'est un export nommé dans TMDBApi.js
@@ -14,13 +14,19 @@ class Search extends React.Component {
         //Ici on va créer les propriétés de notre component custom Search
         this.searchedText = "" // Initialisation de notre donnée searchedText dans le state
         this.state = { 
-            films: [] } 
+            films: [],
+        isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance par de recherche
+     } 
     }
 
     _loadFilms() {
         if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide 
+        this.setState({ isLoading: true }) // Lancement du chargement
           getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-              this.setState({ films: data.results })
+              this.setState({ 
+                  films: data.results,
+                isLoading: false // Arrêt du chargement
+             })
           })
         }
     }
@@ -29,9 +35,21 @@ class Search extends React.Component {
         this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState
     }
 
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large'></ActivityIndicator>
+                    {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large.
+                    Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+                </View>
+            )
+        }
+    }
+
 
     render() {
-        console.log("RENDER")
+        console.log(this.state.isLoading)
         return (
             //Ici on retourne des éléments graphiques de nottre component custom Search
             <View style={styles.main_container}>
@@ -39,6 +57,7 @@ class Search extends React.Component {
                 style={styles.textinput}
                 placeholder='Titre du film'
                 onChangeText={(text) => this._searchTextInputChanged(text)}
+                onSubmitEditing={() => this._loadFilms()}
                 />
                 <Button style={styles.textinput} title='Rechercher' onPress={() => this._loadFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList*/}
@@ -47,6 +66,7 @@ class Search extends React.Component {
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({item}) => <FilmItem film={item}/>}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -64,6 +84,15 @@ const styles = StyleSheet.create( {
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft:5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
