adb6f2ba7d963e5c4f11956062a35dba852f0569
BeatriceBRIAND <b.briand@mistermenuiserie.com>
2020-12-28 15:29:37 +0100

    commit pagination

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,8 +2,8 @@
 
 const API_TOKEN = "d36b8ad6aa17539880394943199077fa";
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
--- Components/Search.js
+++ Components/Search.js
@@ -12,7 +12,9 @@ class Search extends React.Component {
     constructor(props) {
         super(props)
         //Ici on va créer les propriétés de notre component custom Search
-        this.searchedText = "" // Initialisation de notre donnée searchedText dans le state
+        this.searchedText = "" // Initialisation de notre donnée searchedText 
+        this.page = 0 // Compteur pour connaître la page courante
+        this.totalPages = 0 // Nombre de pages totales pour savoir si on a atteint la fin des retours de l'API TMBD
         this.state = { 
             films: [],
         isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance par de recherche
@@ -22,9 +24,11 @@ class Search extends React.Component {
     _loadFilms() {
         if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide 
         this.setState({ isLoading: true }) // Lancement du chargement
-          getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+              this.page = data.page
+              this.totalPages = data.total_pages
               this.setState({ 
-                  films: data.results,
+                  films: [ ...this.state.films, ...data.results ], //ou faire ça: "films: this.state.films.concat(data.results)" le résultat sera pareil
                 isLoading: false // Arrêt du chargement
              })
           })
@@ -33,6 +37,19 @@ class Search extends React.Component {
 
     _searchTextInputChanged(text) {
         this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState
+    }
+
+    _searchFilms() {
+        //Ici on va remettre à zéro les films de notre state
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [],
+        }, () => { // J'utilise la paramètre lenght sur mon tableau de films pour vérifier qu'il y a bien 0 film
+        console.log("Page : " + this.page + " / Totalpages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+        this._loadFilms()
+        })
+        
     }
 
     _displayLoading() {
@@ -49,7 +66,7 @@ class Search extends React.Component {
 
 
     render() {
-        console.log(this.state.isLoading)
+       // console.log(this.state.isLoading)
         return (
             //Ici on retourne des éléments graphiques de nottre component custom Search
             <View style={styles.main_container}>
@@ -57,14 +74,20 @@ class Search extends React.Component {
                 style={styles.textinput}
                 placeholder='Titre du film'
                 onChangeText={(text) => this._searchTextInputChanged(text)}
-                onSubmitEditing={() => this._loadFilms()}
+                onSubmitEditing={() => this._searchFilms()}
                 />
-                <Button style={styles.textinput} title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button style={styles.textinput} title='Rechercher' onPress={() => this._searchFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList*/}
                 <FlatList
                 data={this.state.films}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({item}) => <FilmItem film={item}/>}
+                onEndReachedThreshold={0.5}
+                onEndReached={() => {
+                    if (this.page < this.totalPages){ //On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                    this._loadFilms()
+                    }
+                }}
                 />
                 {this._displayLoading()}
             </View>
