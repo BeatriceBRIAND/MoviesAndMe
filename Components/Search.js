cd824e444d3c42540de2ff3566d2f42b9c995ff0
BeatriceBRIAND <b.briand@mistermenuiserie.com>
2020-12-28 13:20:16 +0100

    commit liste films

--- Components/Search.js
+++ Components/Search.js
@@ -1,6 +1,8 @@
 //Components/Search.js
 import React from 'react'
-import { StyleSheet, Button, TextInput, View } from 'react-native'
+import { StyleSheet, Button, TextInput, View, FlatList } from 'react-native'
+import film from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 
 
@@ -8,15 +10,25 @@ class Search extends React.Component {
     render() {
         return (
             //Ici on retourne des éléments graphiques de nottre component custom Search
-            <View style={{marginTop: 50}}>
+            <View style={styles.main_container}>
                 <TextInput style={styles.textinput} placeholder='Titre du film'/>
                 <Button style={styles.textinput} title='Rechercher' onPress={() => {}}/>
+                {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList*/}
+                <FlatList
+                data={film}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => <FilmItem film={item}/>}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create( {
+    main_container:{
+        flex: 1,
+        marginTop: 50
+    },
     textinput: {
         marginLeft: 5,
         marginRight: 5,
